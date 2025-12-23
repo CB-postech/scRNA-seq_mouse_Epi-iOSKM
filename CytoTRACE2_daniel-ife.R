438bf38c011d836de3356b02362d05d6f1ab8867
Eunjun Choi <choiej@postech.ac.kr>
2025-12-23 09:57:38 +0900

    Add CytoTRACE2 analysis script for wounding reference

---
+++ CytoTRACE2_daniel-ife.R
@@ -0,0 +1,36 @@
+#######
+# CytoTRACE2 for daniel-ife
+#######
+
+
+
+set.seed(1234)
+# Load data ---------------------------------------------------------------
+daniel.ife <- readRDS("./rds/2024/0610/daniel_ife.rds")
+
+
+
+# Import libraries --------------------------------------------------------
+library("CytoTRACE2")
+
+
+
+# Run CytoTRACE -----------------------------------------------------------
+### Run main function ----
+daniel.ife <- cytotrace2(
+  input = daniel.ife, species = "mouse", is_seurat = TRUE, slot_type = "counts",
+  full_model = TRUE, batch_size = 10000, smooth_batch_size = 1000,
+  parallelize_models = TRUE, parallelize_smoothing = TRUE,
+  ncores = NULL, max_pcs = 200, seed = 1234
+)
+daniel.ife@meta.data <- daniel.ife@meta.data %>% 
+  dplyr::rename(
+    cytotrace2.score = "CytoTRACE2_Score",  # absolute score from 0 (diff) to 1 (totipotent)
+    cytotrace2.potency = "CytoTRACE2_Potency",  # potency assignment based on 6-binning of above
+    cytotrace2.relative = "CytoTRACE2_Relative",  # relative score normalized to 0-1
+    cytotrace2.preKNN_score = "preKNN_CytoTRACE2_Score",
+    cytotrace2.preKNN_potency = "preKNN_CytoTRACE2_Potency"
+
+
+
+#####
